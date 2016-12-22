f1c0b70a5eaf72c974c6894a7bf5cf90478ffb6e
Charlie Meyer <cmey63@gmail.com>
2016-12-22 15:41:48 -0500

    add median picker, clean up code

--- depth.js
+++ depth.js
@@ -1,21 +1,37 @@
+//////////////////////
+// Global Resources //
+//////////////////////
+
 var canvas = document.getElementById("plane");
 var ctx = canvas.getContext("2d");
 var point_size = 3;
 var points = [];
 var point_color = "#212121";
+var median_type = parseInt($("#medianpicker").val());
 
-$(document).ready(function(){
+// Register all the event handlers
+$(document).ready(function() {
     clear_points();
     $("#plane").click(handle_click);
     $("#clear_points").click(clear_points);
+    $("#medianpicker").change(function(){
+        median_type = parseInt($(this).val());
+        draw_median();
+    });
 });
 
+////////////////////
+// Event Handlers //
+////////////////////
+
+// Clear the points
 function clear_points() {
     points = [];
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     draw_point(canvas.width / 2, canvas.height / 2, "#F44336", 5);
 }
 
+// Add the point where they clicked, recompute median
 function handle_click(e) {
     var rect = canvas.getBoundingClientRect();
     var x = event.clientX - rect.left;
@@ -28,47 +44,58 @@ function handle_click(e) {
     draw_median();
 }
 
-function draw_point(x, y, color, radius) {
-    // alert("got a point at " + x + " " + y);
-    ctx.fillStyle = color;
-    ctx.beginPath(); //Start path
-    ctx.arc(x, y, radius, 0, Math.PI * 2, true); 
-    ctx.fill();
-    return [x,y];
+// Draws the median of points based on currently chosen definition
+function draw_median() {
+    switch(median_type) {
+        case 0: 
+            draw_mean_point()
+            break;
+        case 1:
+            draw_median_x_y();
+            break;
+        default:
+            alert("bad option for median type used " + median_type);
+    }
 }
 
-// lmao just gonna get the avg tbh
-function draw_median() {
-    // step 1: get that median
+////////////////////////////////////// 
+// Different median implementations //
+//////////////////////////////////////
+
+function draw_mean_point() {
     var mean_x = 0;
     var mean_y = 0;
     var num_points = points.length;
-
-    xs = []
-    ys = []
-
     for (var i = 0; i < num_points; i++) {
         mean_x += points[i][0];
         mean_y += points[i][1];
+    }
+    mean_x = mean_x / num_points;
+    mean_y = mean_y / num_points;
+    draw_point(mean_x, mean_y, "#388E3C", 5);
+}
+
+function draw_median_x_y() {
+    xs = []
+    ys = []
+
+    var num_points = points.length;
+    for (var i = 0; i < num_points; i++) {
         xs.push(points[i][0]);
         ys.push(points[i][1]);
     }
 
     med_x = median(xs);
     med_y = median(ys);
-
-    mean_x = mean_x / num_points;
-    mean_y = mean_y / num_points;
-
-    // step 2: draw that median
-    // alert("median");
-    // print_point(med_x,med_y);
     draw_point(med_x, med_y, "#F44336", 5);
-    // alert("mean");
-    // print_point(mean_x,mean_y);
-    draw_point(mean_x, mean_y, "#388E3C", 5);
 }
 
+
+///////////////
+// Utilities //
+///////////////
+
+// Returns the median element in list of numbers l
 function median(l) {
     l.sort(function(a,b){return a-b});
     console.log(l);
@@ -84,6 +111,16 @@ function median(l) {
     }
 }
 
+// Draw point on canvas at x y with given color and radius
+function draw_point(x, y, color, radius) {
+    ctx.fillStyle = color;
+    ctx.beginPath(); //Start path
+    ctx.arc(x, y, radius, 0, Math.PI * 2, true); 
+    ctx.fill();
+    return [x,y];
+}
+
+// Debug output for a point
 function print_point(x,y) {
     alert("point at (" + x +", " + y + ")");
 }
